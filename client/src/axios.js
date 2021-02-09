9d6fd3612ef89143d2ef5b9f8aa3b01766a441e2
Saurabh Sen <saurabhsensaurabh@gmail.com>
2021-02-09 20:04:33 +0530

    update axios

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL: process.env.baseURL ||'http://localhost:5000'
+    baseURL: 'http://localhost:5000'
 })
 
-module.exports = instance;
+if(process.env.NODE_ENV === 'production'){
+    module.exports = axios
+}else{
+    module.exports = instance;
+}
